df9fac265927f1cd7fef87a48c993d25e694e25f
Wonicon <conciselove@outlook.com>
2016-03-03 13:20:24 +0800

    Update coe script
    Indicate right mem address width

--- mk_text_mem.rb
+++ mk_text_mem.rb
@@ -11,29 +11,27 @@ puts "Width: #{width}"
 puts "Height: #{height}"
 
 File.open(outfile, 'w') do |f|
-    text = File.open(infile, 'r')
-
     bytes = []
-    line_count = 0
-    text.each_byte do |ch|
-        if ch == "\n".ord
-            (width - line_count).times{ bytes.push(" ".ord) }
-            line_count = 0
-        else
-            line_count = line_count + 1
-            bytes.push(ch)
+    File.open(infile, 'r') do |text|
+        line_count = 0
+        text.each_byte do |ch|
+            if ch == "\n".ord
+                (width - line_count).times{ bytes.push(" ".ord) }
+                line_count = 0
+            else
+                line_count = line_count + 1
+                bytes.push(ch)
+            end
         end
     end
 
-    addr_width = Math.log2(bytes.size - 1).floor + 1;
-
-    f.puts(";text size is #{bytes.size}")
-    f.puts(";mem size is #{width * height}")
-    f.puts(";text address width is #{addr_width}")
+    mem_size = width * height;
+    addr_width = Math.log2(mem_size - 1).floor + 1;
+    f.puts(";mem size is #{mem_size}")
+    f.puts(";mem address width is #{addr_width}")
     f.puts("memory_initialization_radix = 16;")
     f.write("memory_initialization_vector = ")
     f.write(bytes.map{ |byte| sprintf("%02x", byte) }.join(", "))
     f.puts(";")
 
-    text.close
 end
